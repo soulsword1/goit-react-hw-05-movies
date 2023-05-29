6151e977bddc9246c443b4219b66555ba0fa9979
Max Pieshkov <49279392+soulsword1@users.noreply.github.com>
2023-05-29 21:51:27 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -6,11 +6,8 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter >
+    <BrowserRouter basename="/goit-react-hw-05-movies">
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
-
-
-// basename="/goit-react-hw-05-movies"
--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,60 +1,3 @@
-// import { useSearchParams, Link, useLocation } from 'react-router-dom';
-// import { useState, useEffect } from 'react';
-// import FetchMovieByTitleAPI from '../services/FetchMovieByTitleAPI';
-
-// export default function Movies() {
-//   const [movies, setMovies] = useState();
-//   const [searchParams, setSearchParams] = useSearchParams();
-//   const query = searchParams.get('query') ?? '';
-//   const location = useLocation();
-
-//   useEffect(() => {
-//     FetchMovieByTitleAPI(query).then(data => setMovies(data.results));
-//   }, [query]);
-
-//   const onSearch = e => {
-//     e.preventDefault();
-//     setSearchParams({ query: e.target.movie.value });
-//   };
-
-//   return (
-//     <>
-//       <form onSubmit={onSearch}>
-//         <input name="movie" />
-//         <button>Search</button>
-//       </form>
-//       {movies && (
-//         <ul>
-//           {movies.map(({ id, title, name }) => {
-//             if (title) {
-//               return (
-//                 <li key={id}>
-//                   <Link to={`${id}`} state={{ from: location }}>
-//                     {title}
-//                   </Link>
-//                 </li>
-//               );
-//             }
-
-//             return (
-//               <li key={id}>
-//                 <Link to={`${id}`}>{name}</Link>
-//               </li>
-//             );
-//           })}
-//         </ul>
-//       )}
-//     </>
-//   );
-// }
-
-
-
-
-
-
-
-
 import { useSearchParams, Link, useLocation } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import FetchMovieByTitleAPI from '../services/FetchMovieByTitleAPI';
@@ -75,10 +18,6 @@ export default function Movies() {
   return (
     <>
     <MoviesForm onSubmit={onSubmit} />
-      {/* <form onSubmit={onSearch}>
-        <input name="movie" />
-        <button>Search</button>
-      </form> */}
       {movies && (
         <ul>
           {movies.map(({ id, title, name }) => {
